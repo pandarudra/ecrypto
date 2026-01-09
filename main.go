ef08e431b3107419bd5e1bc96bd4efc2971182ab
pandarudra <rudrapanda8206@gmail.com>
2026-01-10 00:55:45 +0530

    ci configured

--- main.go
+++ main.go
@@ -4,20 +4,29 @@ package main
 import (
 	"ecrypto/cmd"
 	"ecrypto/ui"
+	"fmt"
 	"os"
 )
 
-func main() {
-	// If no CLI args, run interactive menu
-	if len(os.Args) == 1 {
-		if err := ui.RunInteractiveMenu(); err != nil {
-			os.Exit(1)
-		}
-		return
-	}
+var Version = "dev"
 
-	// Otherwise, use traditional CLI mode
-	if err := cmd.Execute(); err != nil {
-		os.Exit(1)
-	}
+func main() {
+    // Print version if requested
+    if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "-v") {
+        fmt.Printf("ecrypto %s\n", Version)
+        return
+    }
+
+    // If no CLI args, run interactive menu
+    if len(os.Args) == 1 {
+        if err := ui.RunInteractiveMenu(); err != nil {
+            os.Exit(1)
+        }
+        return
+    }
+
+    // Otherwise, use traditional CLI mode
+    if err := cmd.Execute(); err != nil {
+        os.Exit(1)
+    }
 }
